0e814ef6aa4ce8266aa874b5476a0bfb47372ef4
Pelirrojo <Pelirrojo@users.noreply.github.com>
2024-03-10 22:00:01 +0100

    FEAT: New agent to clean files

--- src/helper_file.py
+++ src/helper_file.py
@@ -1,6 +1,15 @@
 import os
+import time
+import shutil
+import logging
+
 from urllib.parse import urlparse
 
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 def create_folder_if_not_exists(folder_path: str) -> None:
     """Create a folder if it doesn't already exist."""
@@ -17,3 +26,34 @@ def get_file_extension(url: str) -> str:
     path = parsed_url.path
     extension = path.split(".")[-1] if "." in path else ""
     return extension
+
+
+def delete_old_subfolders(path):
+    current_time = time.time()
+    hour_ago = current_time - 3600  # 3600 seconds = 1 hour
+
+    for folder_name in os.listdir(path):
+        folder_path = os.path.join(path, folder_name)
+
+        # Check if the path is a directory
+        if os.path.isdir(folder_path):
+            # Get the latest modified time of files within the folder
+            latest_modified_time = max(
+                [
+                    os.path.getmtime(os.path.join(folder_path, file_name))
+                    for file_name in os.listdir(folder_path)
+                    if os.path.isfile(os.path.join(folder_path, file_name))
+                ],
+                default=0,
+            )
+
+            # Delete the subfolder if no new files were created within the last hour
+            if latest_modified_time <= hour_ago:
+                try:
+                    # Remove the subfolder and its contents recursively
+                    #os.rmdir(folder_path)
+                    shutil.rmtree(folder_path, ignore_errors=True)
+                    logger.info(f"Deleted folder: {folder_path}")
+                except OSError as e:
+                    logger.error(f"Error deleting folder: {folder_path}. Reason: {str(e)}")
+
---
+++ src/scheduler.py
@@ -0,0 +1,29 @@
+import os
+import time
+import logging
+
+from timeloop import Timeloop
+from datetime import timedelta
+
+from dotenv import load_dotenv
+from helper_file import delete_old_subfolders
+
+
+# Enable logging
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Load environment variables from .env file
+load_dotenv()
+temporary_folder = os.getenv("TMP_FOLDER")
+
+tl = Timeloop()
+
+@tl.job(interval=timedelta(seconds=10))
+def sample_job_every_2s():
+    logger.info(f"CLEANING photos... current time : {time.ctime()}")
+    delete_old_subfolders(temporary_folder)
+    
+if __name__ == "__main__":
+    tl.start(block=True)
+    
