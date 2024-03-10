f4a2c4e091fecb2ddae2ddd35eac4ebf0b02f6c4
Pelirrojo <Pelirrojo@users.noreply.github.com>
2024-03-10 22:00:01 +0100

    FIX log filenames

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ mkdir -p "/var/log/bot"
 
 log_folder="/var/log/bot"
 current_date=$(date +%Y-%m-%d)
-log_file_bot="$log_folder/a_$current_date.log"
-log_file_scheduler="$log_folder/b_$current_date.log"
+log_file_bot="$log_folder/bot_$current_date.log"
+log_file_scheduler="$log_folder/scheduler_$current_date.log"
 
 # Run scripts
 nohup python3 src/bot.py > "$log_file_bot" 2>&1 &
